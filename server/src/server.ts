a9881be13a76b466855dd0fd1a21a45621c6d5cf
mateusbruno1 <mateusbruno294@gmail.com>
2020-06-04 00:13:54 -0300

    3º dia

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import routes from './routes';
 
-const app = express();
+const app = express()
 
-app.get('/',(req,res)=>{
-  res.json({err:"testes de execução"})
-})
+app.use(cors());
+app.use(express.json()); //use json formtat
+app.use(routes);
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
-app.listen(3333);
-
+app.listen(3333)
